87d98852f93907381b6f82e83a932aa8b7827ffa
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-06-30 20:22:03 +0100

    first commit

---
+++ src/main/java/org/engripaye/containerizedjavaapp/ContainerizedJavaAppApplication.java
@@ -0,0 +1,13 @@
+package org.engripaye.containerizedjavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerizedJavaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContainerizedJavaAppApplication.class, args);
+    }
+
+}
